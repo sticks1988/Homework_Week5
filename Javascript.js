41174d6e04810b5499a4292c296443d5df2fcee3
sticks1988 <mikepb6691@aol.com>
2021-09-06 19:18:47 -0400

    variables and moment.js
    applying variables for my current clock and date for today when loading the page using moment.js applying down to the current second when the page is loaded.
    
    appended my current time and date clock to the header in the class currentDay

--- Javascript.js
+++ Javascript.js
@@ -5,3 +5,11 @@ var hourSign = [
 var listL = [
     "12", "13", "14", "15", "16", "17", "18", "19", "20"
 ];
+
+//This is my current date and time to be shown in the class of currentDay using moment.js
+var squareT = $(".container");
+
+var clockT = moment().format('MMMM Do YYYY, h:mm:ss a');
+
+var hereT = $("#currentDay");
+hereT.append(clockT);
