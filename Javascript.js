881e4448a57752b7965e2074da63f09b94ba678a
sticks1988 <mikepb6691@aol.com>
2021-09-06 20:32:39 -0400

    adding local storage functionality/ fixing background color
    fixed issue with hourSign being in the function for color time that prevented the correct background color to be displayed. gave each else and else if separate lines to read code easier.
    
    Added value to text areas as well as a function to get the information that is stored.
    
    added button function for on click saving purposes when clicking save.
    
    added set items coordinating all the text areas and values set from 0-8 to be displayed in the appropriate block on the page after reloading.

--- Javascript.js
+++ Javascript.js
@@ -27,16 +27,31 @@ for (var i = 0; i < hourSign.length; i++) {
 var verify = setInterval(colorTime);
 
 function colorTime() {
-    var rightNow = moment().hours(hourSign);
+    var rightNow = moment().hours();
     $(".time-block").each(function () {
         var blockHour = parseInt($(this).attr("id").split(" "));
 
         if (blockHour < rightNow) {
             $(this).addClass("past");
         } else if (blockHour === rightNow) {
-            $(this).addClass("present"); $(this).removeClass("past");
+            $(this).addClass("present"); 
+            $(this).removeClass("past");
         } else {
-            $(this).removeClass("present"); $(this).removeClass("past"); $(this).addClass("future");
+            $(this).removeClass("present"); 
+            $(this).removeClass("past"); 
+            $(this).addClass("future");
         }
     });
 }
+grabLocalInformation();
+
+function grabLocalInformation() {
+    $("textarea")[0].value = localStorage.getItem("textarea1"); $("textarea")[1].value = localStorage.getItem("textarea2"); $("textarea")[2].value = localStorage.getItem("textarea3"); $("textarea")[3].value = localStorage.getItem("textarea4"); $("textarea")[4].value = localStorage.getItem("textarea5"); $("textarea")[5].value = localStorage.getItem("textarea6"); $("textarea")[6].value = localStorage.getItem("textarea7"); $("textarea")[7].value = localStorage.getItem("textarea8"); $("textarea")[8].value = localStorage.getItem("textarea9");
+}
+
+$("button").on("click", function (event) {
+    event.preventDefault();
+    var firstBlock = $("textarea")[0].value, secondBlock = $("textarea")[1].value, thirdBlock = $("textarea")[2].value, fourthBlock = $("textarea")[3].value, fifthBlock = $("textarea")[4].value, sixthBlock = $("textarea")[5].value, seventhBlock = $("textarea")[6].value, eighthBlock = $("textarea")[7].value, ninethBlock = $("textarea")[8].value;
+
+    localStorage.setItem("textarea1", firstBlock); localStorage.setItem("textarea2", secondBlock); localStorage.setItem("textarea3", thirdBlock); localStorage.setItem("textarea4", fourthBlock); localStorage.setItem("textarea5", fifthBlock); localStorage.setItem("textarea6", sixthBlock); localStorage.setItem("textarea7", seventhBlock); localStorage.setItem("textarea8", eighthBlock); localStorage.setItem("textarea9", ninethBlock);
+});
